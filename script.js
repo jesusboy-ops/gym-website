9d89ebdf989a393ba0c32a5849cee89ebe0bf7e1
loko israel <israelloko65@gmail.com>
2025-10-18 17:09:49 -0700

    Updated site with optimizations

--- script.js
+++ script.js
@@ -17,42 +17,22 @@ document.addEventListener("DOMContentLoaded", function () {
       640: { slidesPerView: 1 },
       1024: { slidesPerView: 3 },
     },
-    // Optional autoplay (uncomment to enable)
-    // autoplay: {
-    //   delay: 500,
-    //   disableOnInteraction: false,
-    // },
+   
   });
 });
 window.addEventListener("scroll", () => {
   const nav = document.querySelector("nav");
   if (window.scrollY > 100) {
-    nav.classList.add("scrolled"); // add a class when scrolled
+    nav.classList.add("scrolled");
   } else {
     nav.classList.remove("scrolled");
   }
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const elements = document.querySelectorAll(".fade-in");
 
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("show");
-          observer.unobserve(entry.target); // optional: stop observing once shown
-        }
-      });
-    },
-    { threshold: 0.2 }
-  ); // triggers when 20% of the element is visible
-
-  elements.forEach((el) => observer.observe(el));
-});
 
 const swiper = new Swiper('.swiper', {
   slidesPerView: 3,
   spaceBetween: 20,
-  virtual: true, // only renders visible slides
+  virtual: true, 
 });
